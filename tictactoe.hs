6d1fc42424b1993a92538b29e2ac08008a310b69
476458 <476458@LT032871>
2017-02-20 14:48:56 +0530

    State Monad done

--- tictactoe.hs
+++ tictactoe.hs
@@ -1,18 +1,31 @@
 module RL where
 import Control.Monad.State
 import qualified Data.Map as Map
+import Control.Applicative
 
 fun :: Map.Map String Int
 fun = Map.empty
 
-f :: String -> Int-> State (Map.Map String Int) ()
-f row value = do
+store :: String -> Int-> State (Map.Map String Int) ()
+store row value = do
   fun <- get
   put (Map.insert row value fun)
 
-g :: String -> State (Map.Map String Int) (Maybe (Int))
-g roworcolumn = do
+retrieve :: String -> State (Map.Map String Int) (Maybe (Int))
+retrieve roworcolumn = do
   fun <- get
   return (Map.lookup roworcolumn fun) 
 
-main =  print (runState (do {f "row" 1; g "row"}) fun) 
+getrow = do {store "row" 1; retrieve "row"}  
+getcolumn = do {store "column" 1; retrieve "column"}  
+getboardsize = do   
+           let x = (runState getrow fun) in
+             let y = (runState getcolumn fun) in
+                (Just (*) <*> (fst x)  <*>  (fst y) )
+
+main =  do print (runState getrow fun)
+           let x = (runState getrow fun)
+           let y = (runState getcolumn fun)
+           print (getboardsize)
+           return ()
+ 
