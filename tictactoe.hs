4a42058bf5c60fd669b3ed5ca1c48d635baf8627
476458 <476458@LT032871>
2017-02-24 17:04:58 +0530

    Added magic square

--- tictactoe.hs
+++ tictactoe.hs
@@ -2,7 +2,6 @@ module RL where
 import Control.Monad.State
 import qualified Data.Map as Map
 import Control.Applicative
-
 fun :: Map.Map String Int
 fun = Map.empty
 
@@ -10,7 +9,6 @@ store :: String -> Int-> State (Map.Map String Int) ()
 store row value = do
   fun <- get
   put (Map.insert row value fun)
-
 retrieve :: String -> State (Map.Map String Int) (Maybe (Int))
 retrieve roworcolumn = do
   fun <- get
@@ -23,9 +21,19 @@ getboardsize = do
              let y = (runState getcolumn fun) in
                 (Just (*) <*> (fst x)  <*>  (fst y) )
 
+putmagicsquare = do { store "!" 2; store "2" 9;store "3" 4;
+                      store "4" 7; store "5" 5;store "6" 4; 
+                      store "6" 7; store "1" 5;store "8" 4; 
+                    }
+data BoardState = BoardState { first :: [Int],
+                               second :: [Int],
+                               index :: Int
+                             }  deriving (Show)
+
 main =  do print (runState getrow fun)
            let x = (runState getrow fun)
            let y = (runState getcolumn fun)
            print (getboardsize)
            return ()
+ 
  
