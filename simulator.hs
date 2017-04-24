8e2cc3bfc0b97b2f4c8222bd928f47563dcb285f
mohanr <radhakrishnan.mohan@gmail.com>
2017-04-24 22:38:05 +0530

    Not compiling

--- simulator.hs
+++ simulator.hs
@@ -26,5 +26,22 @@ epsilon = do {store "epsilon" 0.1; retrieve "epsilon"}
 runs = do {store "runs" 2000; retrieve "runs"} 
 iterations = do {store "iterations" 3000; retrieve "iterations"} 
 
+runsimulations :: Double -> Matrix R
+runsimulations  alpha = let recordsaver = matrix 1 (runState iterations fun ) * 0 in
+                          let optimalsaver = matrix 1 (runState iterations fun ) * 0 in
+                            let k = (runState karmbandit fun ) in
+                              let bandit = matrix (runState iterations fun ) [k] in
+                                let Q = matrix (runState iterations fun ) [k] in
+                                  let N = matrix (runState runs fun ) [k] in
+                                    simulate 0 Q N
+                                     where
+                                       simulate :: Int -> Int -> Int -> Matrix R
+                                       simulate n Q N=
+                                         case () of _
+                                                      | n < Q -> do
+                                                          randomR (1,N) (mkStdGen 66)
+                                                          matrix 2 [1..6]
+                                                      | n >= Q ->  simulate (x + 1 ) Q N
+
 main = do
   print  (runState karmbandit fun)
