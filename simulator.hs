c8090c7dabcb9271d1d1d24045d717ca8e1cf864
Mohan Radhakrishnan <radhakrishnan.mohan@gmail.com>
2017-04-25 16:26:39 +0530

    First compiling version

--- simulator.hs
+++ simulator.hs
@@ -4,44 +4,28 @@ import Graphics.Matplotlib
 import Control.Monad.State
 import qualified Data.Map as Map
 import Text.Printf
-
-fun :: Map.Map String Double
-fun = Map.empty
+import System.Random
 
 
-store :: String -> Double -> State (Map.Map String Double) ()
-store x value = do
-  fun <- get
-  put (Map.insert x value fun)
 
-retrieve :: String -> State (Map.Map String Double) (Maybe (Double))
-retrieve value = do
-  fun <- get
-  return (Map.lookup  value fun) 
+karmbandit = 10 
 
+alpha = 0.1 
 
-karmbandit = do {store "karmbandit" 10; retrieve "karmbandit"} 
-alpha = do {store "alpha" 0.1; retrieve "alpha"} 
-epsilon = do {store "epsilon" 0.1; retrieve "epsilon"} 
-runs = do {store "runs" 2000; retrieve "runs"} 
-iterations = do {store "iterations" 3000; retrieve "iterations"} 
+epsilon = 0.1 
+
+runs =  2000 
+
+iterations = 3000 
 
 runsimulations :: Double -> Matrix R
-runsimulations  alpha = let recordsaver = matrix 1 (runState iterations fun ) * 0 in
-                          let optimalsaver = matrix 1 (runState iterations fun ) * 0 in
-                            let k = (runState karmbandit fun ) in
-                              let bandit = matrix (runState iterations fun ) [k] in
-                                let Q = matrix (runState iterations fun ) [k] in
-                                  let N = matrix (runState runs fun ) [k] in
-                                    simulate 0 Q N
-                                     where
-                                       simulate :: Int -> Int -> Int -> Matrix R
-                                       simulate n Q N=
-                                         case () of _
-                                                      | n < Q -> do
-                                                          randomR (1,N) (mkStdGen 66)
-                                                          matrix 2 [1..6]
-                                                      | n >= Q ->  simulate (x + 1 ) Q N
-
-main = do
-  print  (runState karmbandit fun)
+runsimulations  alpha = simulate 0 (matrix 1 [iterations] * 0) (matrix 1 [iterations] * 0) iterations karmbandit
+                           where
+                             simulate :: Double -> Matrix R -> Matrix R -> Double-> Double-> Matrix R
+                             simulate x q n iter k=
+                               case () of _
+                                            | x >= iter -> do
+                                                -- randomR (1,k) (mkStdGen 66)
+                                                matrix 2 [1..6]
+                                            | x < iter ->  simulate (x + 1 ) q n iter k
+main = print (runsimulations 0)
