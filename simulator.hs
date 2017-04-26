1ff28942a9ec1a2ddac162df5c17bdfbc69b31f6
Mohan Radhakrishnan <radhakrishnan.mohan@gmail.com>
2017-04-26 14:55:38 +0530

    convertoboolean function

--- simulator.hs
+++ simulator.hs
@@ -12,16 +12,19 @@ karmbandit = 10
 
 alpha = 0.1 
 
-epsilon = 0.1 
-
 runs =  2000 
 
 iterations = 3000 
 
 -- Get matrix of uniformly distributed values
+uniformrand :: Int -> Int ->  IO (Matrix Double)
 uniformrand r c = do
   seed <- randomIO
   return (reshape c $ randomVector seed Uniform (r * c))
+
+converttoboolean :: (Matrix Double) -> IO (Matrix Double)
+converttoboolean m = do
+  return $ step (m - 0.1)
 
 runsimulations :: Double -> IO(Matrix Double)
 runsimulations  alpha = simulate 0 (matrix 1 [iterations] * 0) (matrix 1 [iterations] * 0)
@@ -32,7 +35,8 @@ runsimulations  alpha = simulate 0 (matrix 1 [iterations] * 0) (matrix 1 [iterat
                              simulate x recordsaver optimalsaver iter k q n=
                                case () of _
                                             | x >= iter -> do
-                                                uniformrand 10 10
+                                                m <- uniformrand 10 10
+                                                converttoboolean m
                                             | x < iter ->  simulate (x + 1 ) recordsaver optimalsaver iter k q n
 main = do
   m <- runsimulations 0
